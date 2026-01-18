a777ee9d643b1850900c3698054fd8be18bdfd2e
Vijaya723805 <vijayagaraga84@gmail.com>
2026-01-18 23:23:22 +0530

    Add files via upload

---
+++ Frontend/Sidebar.jsx
@@ -0,0 +1,11 @@
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h3>Profile</h3>
+      <p>Announcements</p>
+      <p>Exams</p>
+      <p>Hackathons</p>
+      <p>Drive</p>
+    </div>
+  );
+}
